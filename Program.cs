e2c5cec2d187e1e08cfa1b31ef5ce1a5c793ce87
Hungpv <phamvanhung124@gmail.com>
2023-05-31 22:15:14 +0700

    dto practice, add auto mapper, increment when add a new character

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 global using csharp002_webapi.Models;
 global using csharp002_webapi.Services.CharacterService;
+global using csharp002_webapi.Dtos.Character;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddScoped<ICharacterService, CharacterService>();
 
 var app = builder.Build();
