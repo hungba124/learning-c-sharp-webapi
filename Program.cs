3745fbe2147d1b2761eb2880deefd006b3f27d5d
Hungpv <phamvanhung124@gmail.com>
2023-06-01 21:25:12 +0700

    add oracle database connection

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 global using csharp002_webapi.Models;
 global using csharp002_webapi.Services.CharacterService;
 global using csharp002_webapi.Dtos.Character;
+global using Microsoft.EntityFrameworkCore;
+global using Oracle.EntityFrameworkCore;
+global using csharp002_webapi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<DataContext>(options => 
+    options.UseOracle(builder.Configuration.GetConnectionString("OracleConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
