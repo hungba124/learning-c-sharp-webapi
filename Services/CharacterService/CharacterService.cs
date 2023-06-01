344a30638fcb0b2c0c43653d24e0bac679408099
hungpv-2151 <pham.van.hung@sun-asterisk.com>
2023-06-01 15:22:56 +0700

    feat: delete character

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -29,6 +29,28 @@ namespace csharp002_webapi.Services.CharacterService
             return serviceResponse;
         }
 
+        public async Task<ServiceResponse<List<GetCharacterDto>>> DeleteCharacter(int id)
+        {
+            var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
+            try
+            {
+                var character = characters.FirstOrDefault(c => c.Id == id);
+
+                if (character is null)
+                    throw new Exception($"Character with Id '{id}' not found");
+
+                characters.Remove(character);
+
+                serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+            }
+            catch (Exception ex)
+            {
+                serviceResponse.Success = false;
+                serviceResponse.Message = ex.Message;
+            }
+            return serviceResponse;
+        }
+
         public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
@@ -58,7 +80,7 @@ namespace csharp002_webapi.Services.CharacterService
                 var character = characters.FirstOrDefault(c => c.Id == updatedCharacter.Id);
 
                 if (character is null)
-                    throw new Exception ($"Character with Id '{updatedCharacter.Id}' not found");
+                    throw new Exception($"Character with Id '{updatedCharacter.Id}' not found");
 
                 _mapper.Map(UpdateCharacter, character);
 
