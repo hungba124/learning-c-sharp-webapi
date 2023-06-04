fa058a3230e0a77556668505dc7b39ea5e86d0bb
Hungpv <phamvanhung124@gmail.com>
2023-06-04 20:42:04 +0700

    refactor: get character from db

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -13,9 +13,11 @@ namespace csharp002_webapi.Services.CharacterService
             new Character { Id = 1, Name = "Sam" }
         };
         private readonly IMapper _mapper;
+        private readonly DataContext _context;
 
-        public CharacterService(IMapper mapper)
+        public CharacterService(IMapper mapper, DataContext context)
         {
+            _context = context;
             _mapper = mapper;
         }
 
@@ -54,16 +56,17 @@ namespace csharp002_webapi.Services.CharacterService
         public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
         {
             var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-            serviceResponse.Data = characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
+            var dbCHaracters = await _context.Characters.ToListAsync();
+            serviceResponse.Data = dbCHaracters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
             return serviceResponse;
         }
 
         public async Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id)
         {
             var serviceResponse = new ServiceResponse<GetCharacterDto>();
-            var character = characters.FirstOrDefault(c => c.Id == id);
-            if (character is not null)
-                serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
+            var dbCharacter = await _context.Characters.FirstOrDefaultAsync(c => c.Id == id);
+            if (dbCharacter is not null)
+                serviceResponse.Data = _mapper.Map<GetCharacterDto>(dbCharacter);
             else
             {
                 serviceResponse.Success = false;
