0dda213c77dade354390c5370b965b0dcb2c1ed8
Hungpv <phamvanhung124@gmail.com>
2023-06-14 23:03:22 +0700

    Update base.sql

--- Data/base.sql
+++ Data/base.sql
@@ -25,27 +25,6 @@ CREATE TABLE [dbo].[Picture](
 ) ON [PRIMARY]
 GO
 
-/****** Object:  Table [dbo].[PictureBinary]    Script Date: 2023-06-14 22:46:27 ******/
-
-CREATE TABLE [dbo].[PictureBinary](
-	[Id] [int] IDENTITY(1,1) NOT NULL,
-	[PictureId] [int] NOT NULL,
-	[BinaryData] [varbinary](max) NULL,
- CONSTRAINT [PK_PictureBinary] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[PictureBinary]  WITH CHECK ADD  CONSTRAINT [FK_PictureBinary_PictureId_Picture_Id] FOREIGN KEY([PictureId])
-REFERENCES [dbo].[Picture] ([Id])
-ON DELETE CASCADE
-GO
-
-ALTER TABLE [dbo].[PictureBinary] CHECK CONSTRAINT [FK_PictureBinary_PictureId_Picture_Id]
-GO
-
 /****** Object:  Table [dbo].[Currency]    Script Date: 2023-06-14 22:45:08 ******/
 
 CREATE TABLE [dbo].[Currency](
